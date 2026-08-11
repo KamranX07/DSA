44ea0c1eed145d85df9fab4d93d29e4137098a56
Md Kamran Akhter <mdkamranakhter78@gmail.com>
2026-08-11 22:38:07 +0530

    Time: 224 ms (8.57%), Space: 47.4 MB (41%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int temp1 = 0;
+        int temp2 = 0;
+        int ans = 0;
+        for(int i=0; i<nums.length; i++) {
+            temp1 += nums[i];
+            temp2 = nums[i];
+            for(int j=i+1; j<nums.length; j++) {
+                temp2 += nums[j];
+            }
+            if(temp1 == temp2) {
+                return i;
+            }          
+        }        
+        return -1;
+    }
+}
